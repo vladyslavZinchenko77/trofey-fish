11fa561e0e746ac1bce709c4cb1a1de682d5d2e9
Vladyslav Zinchenko <vladyslav.zinchenko@gmail.com>
2024-10-21 12:14:30 +0300

    updated lake card

--- src/components/common/LakeCard/index.tsx
+++ src/components/common/LakeCard/index.tsx
@@ -5,54 +5,75 @@ import InstagramIcon from '../IconComponents/InstagramIcon';
 import FacabookIcon from '../IconComponents/FacebookIcon';
 import YoutubeIcon from '../IconComponents/YoutubeIcon';
 
-interface LakeCardProps{
-  id: number
-  lakeName: string
-  instagramLink: string
-  facebookLink: string
-  youtubeLink: string
-  websiteLink: string
-
+interface LakeCardProps {
+  id: number;
+  lakeName: string;
+  instagramLink: string;
+  facebookLink: string;
+  youtubeLink: string;
+  websiteLink: string;
+  coverImg: string;
+  lakeAddress: string
 }
 
-
-const LakeCard: FC<LakeCardProps> = ({id, lakeName='Озеро "Трофейне"', instagramLink='https://www.instagram.com', youtubeLink='https://www.facebook.com/', websiteLink='https://trofeyne.kiev.ua/'}) => {
-  const [onMouse, setOnMouse] = useState(false)
+const LakeCard: FC<LakeCardProps> = ({
+  id,
+  lakeName ,
+  instagramLink,
+  youtubeLink,
+  websiteLink,
+  coverImg ,
+  lakeAddress
+}) => {
+  const [onMouse, setOnMouse] = useState(true);
+  
   return (
-    <div className='flex flex-col w-[400px]'>
+   
+    <div className='flex flex-col w-[400px] relative'>
       <div className='flex justify-center'>
-      <h2>{lakeName}</h2>
+        <h2>{lakeName}</h2>
       </div>
-    <div onMouseEnter={() => setOnMouse(false)} onMouseLeave={() => setOnMouse(true)} className="mt-[10px] bg-[url('/img/lakes/trofeynoe.jpg')] bg-cover bg-center bg-no-repeat cursor-pointer  relative rounded-tl-[50%] rounded-bl-[50%] rounded-br-[50%] w-[400px] h-[350px] hover:rounded-br-[0] hover:rounded-tr-[50%] transition-[2000ms] ">
-      
-      <div className={ onMouse ? "flex absolute right-[10px] bottom-[10px] opacity-[0] transition-[2000ms]" : "flex absolute right-[10px] bottom-[10px] opacity-[1] transition-[2000ms]"}>
-        <a
-          className="ml-[8px]"
-          target="_blank"
-          href={websiteLink}
+      <div
+        onMouseEnter={() => setOnMouse(false)}
+        onMouseLeave={() => setOnMouse(true)}
+        style={{ backgroundImage: `url(${coverImg})` }}
+        className={`mt-[10px] bg-cover bg-center bg-no-repeat cursor-pointer relative rounded-tl-[50%] rounded-bl-[50%] rounded-br-[50%] w-[400px] h-[350px] hover:rounded-br-[0] hover:rounded-tr-[50%] transition-[2000ms]`}
+      >
+        <div
+          className={
+            onMouse
+              ? 'flex absolute right-[10px] bottom-[10px] opacity-[0] transition-[2000ms]'
+              : 'flex absolute right-[10px] bottom-[10px] opacity-[1] transition-[2000ms]'
+          }
         >
-          <WebsiteIcon color="#ffffff" hoverColor="#0275B1" />
-        </a>
-        <a className="ml-[8px]" href={instagramLink}>
-          <InstagramIcon color="#ffffff" hoverColor="#0275B1" />
-        </a>
-        <a
-          className="ml-[8px]"
-          href="https://www.facebook.com/profile.php?id=100001293125330"
-          target="_blank"
-        >
-          <FacabookIcon color="#ffffff" hoverColor="#0275B1" />
-        </a>
-        <a
-          className="ml-[8px]"
-          href={youtubeLink}
-          target="_blank"
-        >
-          <YoutubeIcon color="white" hoverColor="#0275B1" />
-        </a>
+          <a className='ml-[8px]' target='_blank' href={websiteLink}>
+            <WebsiteIcon color='#ffffff' hoverColor='#0275B1' />
+          </a>
+          <a className='ml-[8px]' href={instagramLink}>
+            <InstagramIcon color='#ffffff' hoverColor='#0275B1' />
+          </a>
+          <a
+            className='ml-[8px]'
+            href='https://www.facebook.com/profile.php?id=100001293125330'
+            target='_blank'
+          >
+            <FacabookIcon color='#ffffff' hoverColor='#0275B1' />
+          </a>
+          <a className='ml-[8px]' href={youtubeLink} target='_blank'>
+            <YoutubeIcon color='white' hoverColor='#0275B1' />
+          </a>
+        </div>
       </div>
+      <p
+        className={`right-0 text-center  p-2 transition-all duration-1000 ease-in-out ${
+          !onMouse ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
+        }`}
+      >
+        {lakeAddress}
+      </p>
+    
     </div>
-    </div>
+    
   );
 };
 
--- src/components/common/LakeCardList/index.tsx
+++ src/components/common/LakeCardList/index.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from 'react';
 import LakeCard from '../LakeCard';
 
+
+const lakes = [
+  {id: 1,
+    lakeName: 'Озеро "Трофейне"',
+    instagramLink: 'https://www.instagram.com',
+    facebookLink: 'https://www.facebook.com/',
+    youtubeLink: '',
+    websiteLink: 'https://trofeyne.kiev.ua/',
+    coverImg: '/img/lakes/trofeynoe.jpg',
+    lakeAddress : 'Озеро "Трофейне", с. Личанка, Київська обл., Києво-Святошинський р-н'
+  }
+]
+
+
 const LakeCardList: FC = () => {
   return (
     <div className="mt-[24px] ml-[40px] mr-[40px]">
-      <h2>LakeCardList: </h2>
-      <div>
-        <LakeCard />
+      <h2 className='text-center semi-bold'>Озера для Риболовлі Київщини</h2>
+      <div className='mt-[24px]'>
+        {lakes.map((lake=>(
+          <LakeCard key={lake.id} id={lake.id} coverImg={lake.coverImg} facebookLink={lake.facebookLink} instagramLink={lake.instagramLink} lakeName={lake.lakeName} websiteLink={lake.websiteLink} youtubeLink={lake.youtubeLink} lakeAddress={lake.lakeAddress}/>
+        )))}
       </div>
     </div>
   );
