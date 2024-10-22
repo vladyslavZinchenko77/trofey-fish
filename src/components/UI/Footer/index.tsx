3e2a2e7f9d2625d6c51135b4a5f30a06f2fe2627
Vladyslav Zinchenko <vladyslav.zinchenko@gmail.com>
2024-10-22 15:33:05 +0300

    updated footer styles

--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import Image from 'next/image';
+import InstagramIcon from '@/components/common/IconComponents/InstagramIcon';
+import FacebookIcon from '@/components/common/IconComponents/FacebookIcon';
 
 const Footer: FC = () => {
   return (
     <footer className="relative mt-[24px] ">
-      <div className="absolute z-2 top-0 left-0 right-0 w-full h-[15%] bg-gradient-to-b from-white to-transparent"></div>
       <Image
         className="w-full h-auto"
         src={'/svg/Lake-background.svg'}
@@ -12,6 +13,21 @@ const Footer: FC = () => {
         height={692}
         alt="fishmen on the lake"
       />
+      <div className="flex items-center m-[12px] absolute left-0 right-0 bottom-0 justify-between">
+        <p>© Всі права захищені 2024</p>
+        <div className="flex">
+          <a className="ml-[8px]" href="" target="_blank">
+            <InstagramIcon hoverColor='white' />
+          </a>
+          <a className="ml-[8px]" href="" target="_blank">
+            <FacebookIcon hoverColor='white' />
+          </a>
+        </div>
+        <div className="flex flex-col">
+          <a href="">Політика конфіденційності</a>
+          <a href="">Повідомлення про куки</a>
+        </div>
+      </div>
     </footer>
   );
 };
