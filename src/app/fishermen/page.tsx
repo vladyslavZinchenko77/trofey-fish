c1613a43a1b0252feae0e4a25e63f24d12dd463a
vladyslavZinchenko77 <vladyslav.zinchenko@gmail.com>
2024-12-20 12:52:55 +0200

    some updates

--- src/app/fishermen/page.tsx
+++ src/app/fishermen/page.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 import Header from '@/components/UI/Header';
 import Footer from '@/components/UI/Footer';
+import NotFound from '@/components/UI/Sections/NotFound';
 
 const FishermenPage: FC = () => {
   return (
     <>
       <Header />
-      <main>
-        <div>FishermenPage</div>
+      <main className="min-h-screen flex items-center justify-center">
+        <NotFound />
       </main>
       <Footer />
     </>
