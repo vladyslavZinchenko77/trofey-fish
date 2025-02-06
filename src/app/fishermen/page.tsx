736a366345e87f6a8355948f13d6493c72210321
vladyslavZinchenko77 <vladyslav.zinchenko@gmail.com>
2025-02-06 23:02:14 +0200

    fixed fishermen pages layout

--- src/app/fishermen/page.tsx
+++ src/app/fishermen/page.tsx
@@ -6,11 +6,13 @@ import NotFound from '@/components/UI/Sections/NotFound';
 const FishermenPage: FC = () => {
   return (
     <>
+    <div className='flex flex-col justify-between min-h-[100vh]'>
       <Header />
       <main className="flex items-center justify-center">
         <NotFound type="inProgress" />
       </main>
       <Footer />
+      </div>
     </>
   );
 };
