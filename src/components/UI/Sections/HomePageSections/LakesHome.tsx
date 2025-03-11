e346a300e26b3b06bf1f6e0fcdd7695bd1053acc
vladyslavZinchenko77 <vladyslav.zinchenko@gmail.com>
2025-03-11 18:15:36 +0200

    added video instead img for bg in the lakes section

--- src/components/UI/Sections/HomePageSections/LakesHome.tsx
+++ src/components/UI/Sections/HomePageSections/LakesHome.tsx
@@ -3,14 +3,14 @@ import ButtonWithFishAnimation from '@/components/common/ButtonWithFishAnimation
 
 const LakesHome: FC = () => {
   return (
-    <section
-      className="relative h-[300px] sm:h-[400px] md:h-[500px] flex flex-col justify-center items-center text-white text-center px-4 sm:px-6 md:px-8"
-      style={{
-        backgroundImage: "url('/webp/lake-home.webp')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+    <section className="relative h-[300px] sm:h-[400px] md:h-[500px] flex flex-col justify-center items-center text-white text-center px-4 sm:px-6 md:px-8">
+      <div className="absolute inset-0 overflow-hidden">
+        <video autoPlay loop muted className="object-cover w-full h-full">
+          <source src="/video/lake-hd_1920_1080_24fps.mp4" type="video/mp4" />
+          Ваш браузер не поддерживает видео.
+        </video>
+      </div>
+
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       <div className="relative z-10 max-w-lg sm:max-w-xl md:max-w-2xl flex flex-col items-center">
